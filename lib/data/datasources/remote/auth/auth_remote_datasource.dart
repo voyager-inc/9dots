1e3e939382fcc0ba7819868cfe7d0a8316c73c84
Tan Le <tanlnd@voyager-hcm.com>
2024-02-06 17:56:23 +0700

    Divide auth remote datasource file into two files auth remote datasource and its implementation

--- lib/data/datasources/remote/auth/auth_remote_datasource.dart
+++ lib/data/datasources/remote/auth/auth_remote_datasource.dart
@@ -1,25 +1,6 @@
-import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:fpdart/fpdart.dart';
-import 'package:ninedots/core/network/api_client.dart';
 import 'package:ninedots/core/utils/app_exception.dart';
 
-abstract class AuthDatasource {
+abstract class AuthRemoteDatasource {
   Future<Either<AppException, bool>> isSessionLogged();
-}
-
-class AuthRemoteDatasource extends AuthDatasource {
-  final ApiClient apiClient;
-  AuthRemoteDatasource(this.apiClient);
-
-  @override
-  Future<Either<AppException, bool>> isSessionLogged() async {
-    final response = await apiClient.fetchSession();
-    return response.fold(
-      (l) => Left(l),
-      (session) {
-        final CognitoAuthSession result = session as CognitoAuthSession;
-        return Right(result.isSignedIn);
-      },
-    );
-  }
 }
